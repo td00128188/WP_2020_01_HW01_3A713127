48a3bffd8a10134312b4bf3b2ca2bfcf55471578
許福陞 <zxcv81602@gmail.com>
2020-11-08 18:22:52 +0800

    MD5

--- WP_2020_01_HW01_3A713127/Form1.Designer.cs
+++ WP_2020_01_HW01_3A713127/Form1.Designer.cs
@@ -28,21 +28,62 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.tb1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.rtb1 = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
+            // 
+            // tb1
+            // 
+            this.tb1.Font = new System.Drawing.Font("新細明體", 20F);
+            this.tb1.Location = new System.Drawing.Point(197, 164);
+            this.tb1.Multiline = true;
+            this.tb1.Name = "tb1";
+            this.tb1.Size = new System.Drawing.Size(346, 55);
+            this.tb1.TabIndex = 0;
+            this.tb1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 20F);
+            this.label1.Location = new System.Drawing.Point(283, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(185, 34);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "請輸入名字";
+            // 
+            // rtb1
+            // 
+            this.rtb1.Font = new System.Drawing.Font("新細明體", 16F);
+            this.rtb1.Location = new System.Drawing.Point(197, 251);
+            this.rtb1.Name = "rtb1";
+            this.rtb1.Size = new System.Drawing.Size(346, 246);
+            this.rtb1.TabIndex = 2;
+            this.rtb1.Text = "";
+            this.rtb1.TextChanged += new System.EventHandler(this.rtb1_TextChanged);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(782, 553);
+            this.Controls.Add(this.rtb1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tb1);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox tb1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox rtb1;
     }
 }
 
--- WP_2020_01_HW01_3A713127/Form1.cs
+++ WP_2020_01_HW01_3A713127/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,14 +13,33 @@ namespace WP_2020_01_HW01_3A713127
 {
     public partial class Form1 : Form
     {
+        string[] name = new string[] { };
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void textBox1_TextChanged(object sender, EventArgs e)
         {
+            
+        }
 
+        private void rtb1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+    public static class MD5Extensions
+    {
+        public static string ToMD5(this string str)
+        {
+            using (var cryptoMD5 = System.Security.Cryptography.MD5.Create())
+            {
+                byte[] source = Encoding.Default.GetBytes(str);
+                byte[] crypto = cryptoMD5.ComputeHash(source);
+                string result = Convert.ToBase64String(crypto);
+                return result;
+            }
         }
     }
 }
